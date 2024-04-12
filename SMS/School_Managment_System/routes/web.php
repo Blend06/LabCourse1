f59091deb53ee87595ed6ed463ff1e89b63ed0cb
Aldin64730 <as64730@ubt-uni.net>
2024-04-12 21:05:18 +0200

    e ndreqa homepage

--- SMS/School_Managment_System/routes/web.php
+++ SMS/School_Managment_System/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('Homepage react/src/pages/FrameOne/index');
 });
 
 Route::get('/dashboard', function () {
