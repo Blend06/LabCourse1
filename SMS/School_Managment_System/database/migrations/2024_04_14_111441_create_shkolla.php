054370be7241fae0ea4459cd15fe2f8aa983c762
Aldin64730 <as64730@ubt-uni.net>
2024-04-14 13:17:20 +0200

    Databaza e projektit n'phpmyadmin

---
+++ SMS/School_Managment_System/database/migrations/2024_04_14_111441_create_shkolla.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shkolla', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Emri');
+            $table->string('Qyteti');
+            $table->string('Rruga');
+            $table->integer('Zip-code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shkolla');
+    }
+};
