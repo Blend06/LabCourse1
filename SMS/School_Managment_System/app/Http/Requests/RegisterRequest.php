b8306276bdbf40a6080263f14e2d199777ab2011
Blend06 <bk67418@ubt-uni.net>
2024-05-27 12:48:05 +0200

    ndryshime njson file

--- SMS/School_Managment_System/app/Http/Requests/RegisterRequest.php
+++ SMS/School_Managment_System/app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password; // Import the Password class
+
 
 class RegisterRequest extends FormRequest
 {
