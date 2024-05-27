fe1dc40fcf6ddcf46018b059c895d4ee895836c6
Aldin64730 <as64730@ubt-uni.net>
2024-05-27 14:30:30 +0200

    ndreqja e api

--- SMS/School_Managment_System/react/src/axios-client.js
+++ SMS/School_Managment_System/react/src/axios-client.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);  // Add this line to log the API Base URL
+console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);  
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}`
 });
 
 axiosClient.interceptors.request.use((config) => {
